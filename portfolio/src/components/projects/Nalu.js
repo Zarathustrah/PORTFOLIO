9d402efd4e61d7e52dbd6ea77d33e05b77f23aff
Dan Price <danielparkerprice@gmail.com>
2020-11-14 23:35:07 +0200

    static setup

--- portfolio/src/components/projects/Nalu.js
+++ portfolio/src/components/projects/Nalu.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import { makeStyles } from '@material-ui/styles'
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    minHeight: '40vh',
+    backgroundImage: `url(${process.env.PUBLIC_URL + '/assets/Nalu.jpg'})`,
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover'
+  }
+}))
 
 export default function Nalu() {
+  const classes = useStyles()
   return(
-    <>
-    <h1>Nalu</h1>
-    </>
+    <div className={classes.root}>
+    </div> 
   )
 }
 
