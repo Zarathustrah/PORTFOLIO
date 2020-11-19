8dcecad0ccde5e9c10b3532df21aa504f5649e29
Dan Price <danielparkerprice@gmail.com>
2020-11-19 11:44:52 +0000

    responsive grid

--- portfolio/src/components/projects/Projects2.js
+++ portfolio/src/components/projects/Projects2.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/core/styles'
+import { useTheme } from '@material-ui/core/styles'
 import GridList from '@material-ui/core/GridList'
 import GridListTile from '@material-ui/core/GridListTile'
-import { createMuiTheme, ThemeProvider, useTheme } from '@material-ui/core/styles';
-import useMediaQuery from '@material-ui/core/useMediaQuery';
+import useMediaQuery from '@material-ui/core/useMediaQuery'
 import projectData from '../../static/projectData'
 
 
@@ -25,30 +25,30 @@ const useStyles = makeStyles((theme) => ({
 function ImageGridList() {
   const classes = useStyles()
   const theme = useTheme()
-  const matches = useMediaQuery(theme.breakpoints.up('sm'))
+
+  const screenSmall = useMediaQuery(theme.breakpoints.only('sm'))
+
+  const getScreenWidth = () => {
+    if (screenSmall) {
+      return 12
+    }
+  }
   
 
   return (
       <div className={classes.root}>
+        
         <GridList cellHeight={475} className={classes.gridList} cols={12}>
         {projectData.map((tile) => (
           <GridListTile key={tile.image} cols={tile.cols}>
             <img src={tile.image} alt={tile.name} />
             <div>{tile.description}</div>
-            <span>{`theme.breakpoints.down('sm') matches: ${matches}`}</span>
           </GridListTile>
         ))}
-        </GridList>     
+        </GridList>    
+
     </div>
   );
 }
 
-const theme = createMuiTheme()
-
-export default function ThemeHelper() {
-  return (
-    <ThemeProvider theme={theme}>
-      <ImageGridList />
-    </ThemeProvider>
-  );
-}
+export default ImageGridList
