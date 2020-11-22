64dc96309d11101e711c0dcd1485f7bb68490287
Dan Price <danielparkerprice@gmail.com>
2020-11-22 22:17:42 +0000

    ud

--- portfolio/src/components/projects/Screened.js
+++ portfolio/src/components/projects/Screened.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Card, CardMedia, Typography } from '@material-ui/core'
+import { Card, CardMedia } from '@material-ui/core'
 import { makeStyles } from '@material-ui/styles'
 
 const useStyles = makeStyles({
