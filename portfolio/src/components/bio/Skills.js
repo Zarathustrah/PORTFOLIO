8cafb7ccd5d07a0bdc71253653489caafa664d99
Dan Price <danielparkerprice@gmail.com>
2020-11-26 11:05:53 +0000

    skills

--- portfolio/src/components/bio/Skills.js
+++ portfolio/src/components/bio/Skills.js
@@ -3,7 +3,7 @@ import { makeStyles } from '@material-ui/core/styles'
 import { Box, Grid } from '@material-ui/core'
 import { IconContext } from "react-icons"
 
-import { 
+import {
   SiHtml5,
   SiCss3,
   SiMaterialUi,
@@ -17,17 +17,20 @@ import {
   SiSass,
   SiGithub,
   SiYarn,
-  SiNpm, 
+  SiNpm,
   SiHeroku,
-  SiJavascript 
+  SiJavascript
 } from "react-icons/si"
 
 import { FaExternalLinkSquareAlt } from "react-icons/fa"
 
 const useStyles = makeStyles({
   root: {
-    backgroundColor: '#1f26ff' 
-  },  
+    backgroundColor: '#1f26ff'
+  },
+  wrapper: {
+    paddingTop: '5px',
+  },
   icon: {
     display: 'flex',
     flexDirection: 'column',
@@ -39,6 +42,7 @@ const useStyles = makeStyles({
     fontSize: '8px',
     textAlign: 'center',
     margin: '20px',
+    // padding: '5px'
     // border: '1px solid',
     // borderRadius: '12.5%'
   },
@@ -52,58 +56,75 @@ export default function Skills() {
 
   return (
     <div className={classes.root}>
-      <Grid 
+      <Grid
         container
         direction='row'
-        justify='center'>
-          <IconContext.Provider value={{ size: '1.5rem' }}>
-            <div className={classes.icon}>
-              <SiHtml5 />
-              <p className={classes.text}>HTML</p>
-            </div>
-            <div className={classes.icon}>
-              <SiCss3 />
-              <p className={classes.text}>CSS</p>
-            </div>
-            <div className={classes.icon}>
-              <SiSass />
-              <p className={classes.text}>Sass</p>
-            </div>
-            <div className={classes.icon}>
-              <SiJavascript />
-              <p className={classes.text}>JavaScript</p>
-            </div>
-            <div className={classes.icon}>
-              <SiReact />
-              <p className={classes.text}>React</p>
-            </div>
-            <div className={classes.icon}>
-              <SiNodeDotJs />
-              <p className={classes.text}>NodeJS</p>
-            </div>
-            <div className={classes.icon}>
-              <SiMongodb />
-              <p className={classes.text}>MongoDB</p>
-            </div>
-            <div className={classes.icon}>
-              <SiMaterialUi />
-              <p className={classes.text}>MaterialUI</p>
-            </div>
-            <div className={classes.icon}>
-              <SiPython />
-              <p className={classes.text}>Python</p>
-            </div>
-            <div className={classes.icon}>
-              <SiDjango />
-              <p className={classes.text}>Django</p>
-            </div>
-            <div className={classes.icon}>
-              <SiPostgresql />
-              <p className={classes.text}>PostgresSQL</p>
-            </div>
-
-          </IconContext.Provider>
-        </Grid>
-   </div>    
+        justify='center'
+        className={classes.wrapper}
+        >
+        <IconContext.Provider value={{ size: '1.5rem' }}>
+          <div className={classes.icon}>
+            <SiHtml5 />
+            <p className={classes.text}>HTML</p>
+          </div>
+          <div className={classes.icon}>
+            <SiCss3 />
+            <p className={classes.text}>CSS</p>
+          </div>
+          <div className={classes.icon}>
+            <SiSass />
+            <p className={classes.text}>Sass</p>
+          </div>
+          <div className={classes.icon}>
+            <SiJavascript />
+            <p className={classes.text}>JavaScript</p>
+          </div>
+          <div className={classes.icon}>
+            <SiReact />
+            <p className={classes.text}>React</p>
+          </div>
+          <div className={classes.icon}>
+            <SiNodeDotJs />
+            <p className={classes.text}>NodeJS</p>
+          </div>
+          <div className={classes.icon}>
+            <SiMongodb />
+            <p className={classes.text}>MongoDB</p>
+          </div>
+          <div className={classes.icon}>
+            <SiMaterialUi />
+            <p className={classes.text}>MaterialUI</p>
+          </div>
+          <div className={classes.icon}>
+            <SiPython />
+            <p className={classes.text}>Python</p>
+          </div>
+          <div className={classes.icon}>
+            <SiDjango />
+            <p className={classes.text}>Django</p>
+          </div>
+          <div className={classes.icon}>
+            <SiPostgresql />
+            <p className={classes.text}>PostgresSQL</p>
+          </div>
+          <div className={classes.icon}>
+            <SiYarn />
+            <p className={classes.text}>Yarn</p>
+          </div>
+          <div className={classes.icon}>
+            <SiNpm />
+            <p className={classes.text}>NPM</p>
+          </div>
+          <div className={classes.icon}>
+            <SiGit />
+            <p className={classes.text}>Git</p>
+          </div>
+          <div className={classes.icon}>
+            <SiGithub />
+            <p className={classes.text}>GitHub</p>
+          </div>
+        </IconContext.Provider>
+      </Grid>
+  </div>
   )
 }
